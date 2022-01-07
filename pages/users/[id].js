8eccb54221d053ecf1d9401e5ef10c4d39b50d40
Alexander Holder <alex@hindsiteind.com>
2022-01-07 10:00:26 +1000

    First commit

---
+++ pages/users/[id].js
@@ -0,0 +1,21 @@
+import { useRouter } from 'next/router'
+
+export default function Car({ user }) {
+    const router = useRouter()
+    const { id } = router.query
+
+    return (<>
+        <h1>Hello {id}</h1>
+        <img src={user.image} />
+    </>)
+    
+}
+
+export async function getServerSideProps({ params }) {
+    const req = await fetch(`http://localhost:3000/${params.id}.json`);
+    const data = await req.json();
+
+    return {
+        props: { user: data },
+    }
+}
